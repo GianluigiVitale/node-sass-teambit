805c85cb4d0def472acad5832684463a92b00c35
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-04 20:13:07 +0200

    Dropdown Menu
    when you click the hamburgher icon the dropdown menu shows up and the icon chances to an X, when you click the X the dropdown menu hides and it shows back the hamburgher icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 $('.remove').click(function () {    // al click di .remove nascondo la scritta dei cookies
     $('.cookies').hide();
 });
+
+$('.icona .fa-bars').click(function () {    // quando si clicca l'hamburgher mostra l'icona della X e il menu a tendina
+    $(this).hide();
+    $('.icona .fa-times').show();
+    $('.hamburger-menu').show();
+});
+
+$('.icona .fa-times').click(function () {   // quando si clicca la X nasconde il menu a tendina e mostra l'hamburgher
+    $(this).hide();
+    $('.icona .fa-bars').show();
+    $('.hamburger-menu').hide();
+});
