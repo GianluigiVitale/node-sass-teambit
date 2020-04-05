c9a32c48201fc20e1b0f2fa554340fc620e2f358
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-05 12:51:34 +0200

    Started working on support section (see desc.)
    -Created support icon with animations (on click of the support icon it changes to an X and with another click it shows again the support icon). Two different animations when showing the two icons.
    -Created function to show dropdown menu

--- dist/main.js
+++ dist/main.js
@@ -108,18 +108,27 @@ $('.remove').click(function () {
   // al click di .remove nascondo la scritta dei cookies
   $('.cookies').hide();
 });
-$('.icona .fa-bars').click(function () {
-  // quando si clicca l'hamburgher mostra l'icona della X e il menu a tendina
-  $(this).hide();
-  $('.icona .fa-times').show();
-  $('.hamburger-menu').show();
-});
-$('.icona .fa-times').click(function () {
-  // quando si clicca la X nasconde il menu a tendina e mostra l'hamburgher
-  $(this).hide();
-  $('.icona .fa-bars').show();
-  $('.hamburger-menu').hide();
-});
+mostraNascondiMenuATendina();
+$('.icon-support').click(function () {
+  // al click di '.icon-support' mostra l'icona della X o l'altra
+  $(this).children().toggleClass('active').toggleClass('hidden');
+}); // FUNZIONI UTILIZZATE
+
+function mostraNascondiMenuATendina() {
+  // // questa funzione al click dell'icona dell'hamburgher mostra il menu a tendina e l'icona della X mentre quando si clicca la X nasconde il menu a tendina e mostra l'hamburgher
+  $('.icona .fa-bars').click(function () {
+    // quando si clicca l'hamburgher mostra l'icona della X e il menu a tendina
+    $(this).hide();
+    $('.icona .fa-times').show();
+    $('.hamburger-menu').show();
+  });
+  $('.icona .fa-times').click(function () {
+    // quando si clicca la X nasconde il menu a tendina e mostra l'hamburgher
+    $(this).hide();
+    $('.icona .fa-bars').show();
+    $('.hamburger-menu').hide();
+  });
+}
 
 /***/ }),
 
